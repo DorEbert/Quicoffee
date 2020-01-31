6b4d7a10632a97d152d8dafd7b14a00ab502490e
DorEbert <dorebert1@gmail.com>
2020-02-01 01:17:24 +0200

    add UI to LoginActivity and add sharedPreference fileName -> used for login only once to the appliaction

--- app/src/main/java/com/example/quicoffee/Global_Variable.java
+++ app/src/main/java/com/example/quicoffee/Global_Variable.java
@@ -2,7 +2,12 @@ package com.example.quicoffee;
 
 class Global_Variable {
     public static final String PASSWORD = "Password";
+    public static final String EMAIL = "Email";
+    public static final String LOGIN = "Login";
     public static int index = 0;
+    // in order to save shared preferences-> used to login only once 
+    public static final String PREFS_NAME = "MyPrefsFile";
+    // in order to generate a unique ID for every created view
     public static int GetID() {
         return index++;
     }
--- app/src/main/java/com/example/quicoffee/MainActivity.java
+++ app/src/main/java/com/example/quicoffee/MainActivity.java
@@ -3,11 +3,14 @@ package com.example.quicoffee;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.ViewGroup;
+import android.view.Gravity;
 import android.view.inputmethod.EditorInfo;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+
+import javax.microedition.khronos.opengles.GL;
 
 public class MainActivity extends AppCompatActivity {
     private int usernameTextboxID;
@@ -21,12 +24,34 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        BuildActivityUI();
+
+    }
+    private void BuildActivityUI(){
+        LinearLayout.LayoutParams lparams = new LinearLayout.LayoutParams((int)(mainActivityWitdh *0.9),mainActivityHeight/20);
+        //Email label and textBox
+        TextView emailTextView = CreateTextView(Global_Variable.EMAIL);
+        EditText emailEditText = CreateEditText(lparams);
+        emailEditText.setId(Global_Variable.GetID());
+        usernameTextboxID = emailEditText.getId();
+        //Password label and textBox
+        TextView passwordTextViiew = CreateTextView(Global_Variable.PASSWORD);
+        EditText passwordEditText = CreateEditText(lparams);
+        passwordEditText.setId(Global_Variable.GetID());
+        passwordTextboxID = passwordEditText.getId();
+        //Add Views to LinearLayout
+        linearLayout.addView(emailTextView);
+        linearLayout.addView(emailEditText);
+        linearLayout.addView(passwordTextViiew);
+        linearLayout.addView(passwordEditText);
+        addLoginButton();
     }
     private void InititalVariablesOfLocalActivity(){
         mainActivityWitdh = getResources().getDisplayMetrics().widthPixels;
         mainActivityHeight = getResources().getDisplayMetrics().heightPixels;
+        linearLayout = findViewById(R.id.linear_layout);
     }
-    private EditText createTextBox(LinearLayout.LayoutParams lparams) {
+    private EditText CreateEditText(LinearLayout.LayoutParams lparams) {
         //Set EditText Setting
         EditText editText = new EditText(this);
         editText.setMaxLines(1);
@@ -35,13 +60,29 @@ public class MainActivity extends AppCompatActivity {
         editText.setLayoutParams(lparams);
         return editText;
     }
-
-    public void addLabel(String labelText){
+    private void addLoginButton() {
+        //Set Button Settings
+        Button loginButton = new Button(this);
+        //loginButton.setOnClickListener(this);
+        loginButton.setText(Global_Variable.LOGIN);
+        LinearLayout.LayoutParams loginButtonLayoutParams =
+                new LinearLayout.LayoutParams((int)(mainActivityWitdh *0.5),mainActivityHeight/20);
+        loginButtonLayoutParams.gravity = Gravity.CENTER;
+        loginButtonLayoutParams.setMargins(0
+                ,mainActivityHeight/20
+                ,0
+                ,mainActivityHeight/40);
+        loginButton.setLayoutParams(loginButtonLayoutParams);
+        loginButton.setBackgroundResource(R.color.loginViewColor);
+        loginButton.setTextColor(getApplication().getResources().getColor(R.color.colorBlack));
+        linearLayout.addView(loginButton);
+    }
+    private TextView CreateTextView(String labelText){
         //Set Label Setting
         TextView textView = new TextView(this);
         textView.setText(labelText);
         textView.setTextColor(getApplication().getResources().getColor(R.color.colorBlack));
         textView.setTextSize(mainActivityWitdh/40);
-        linearLayout.addView(textView);
+        return textView;
     }
 }
