35b94634e5ce10c427c982fcbcc759db90da3f5a
DorEbert <dorebert1@gmail.com>
2020-02-01 00:29:11 +0200

    add Global Variable class, add colors to color.xml and initial login activity

---
+++ app/src/main/java/com/example/quicoffee/Global_Variable.java
@@ -0,0 +1,9 @@
+package com.example.quicoffee;
+
+class Global_Variable {
+    public static final String PASSWORD = "Password";
+    public static int index = 0;
+    public static int GetID() {
+        return index++;
+    }
+}
--- app/src/main/java/com/example/quicoffee/MainActivity.java
+++ app/src/main/java/com/example/quicoffee/MainActivity.java
@@ -3,12 +3,39 @@ package com.example.quicoffee;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.ViewGroup;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private int usernameTextboxID;
+    private int passwordTextboxID;
+    private int mainActivityWitdh;
+    private LinearLayout linearLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    private EditText createTextBox(LinearLayout.LayoutParams lparams) {
+        //Set EditText Setting
+        EditText editText = new EditText(this);
+        editText.setMaxLines(1);
+        editText.setImeOptions(EditorInfo.IME_ACTION_NEXT);
+        editText.setBackgroundColor(getApplication().getResources().getColor(R.color.textViewColor));
+        editText.setLayoutParams(lparams);
+        return editText;
+    }
+
+    public void addLabel(String labelText){
+        //Set Label Setting
+        TextView textView = new TextView(this);
+        textView.setText(labelText);
+        textView.setTextColor(getApplication().getResources().getColor(R.color.colorBlack));
+        textView.setTextSize(mainActivityWitdh/40);
+        linearLayout.addView(textView);
     }
 }
