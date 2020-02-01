c378362c485ac3a03cef1dac9ad11c2a5dab20a9
DorEbert <dorebert1@gmail.com>
2020-02-01 17:15:47 +0200

    rename MainActivity to LoginActivity and add auth login to fireBase

--- app/src/main/java/com/example/quicoffee/Global_Variable.java
+++ app/src/main/java/com/example/quicoffee/Global_Variable.java
@@ -4,6 +4,10 @@ class Global_Variable {
     public static final String PASSWORD = "Password";
     public static final String EMAIL = "Email";
     public static final String LOGIN = "Login";
+    public static final String MISSING_EMAIL_INFORMATION = "Enter email address!";
+    public static final String MISSING_PASSWORD_INFORMATION = "Enter password!";
+    public static final int MINIMUM_PASSWORD = 6;
+    public static final String AUTH_FAILED = "Authorization failed";
     public static int index = 0;
     // in order to save shared preferences-> used to login only once
     public static final String PREFS_NAME = "MyPrefsFile";
--- app/src/main/java/com/example/quicoffee/LoginActivity.java
+++ app/src/main/java/com/example/quicoffee/LoginActivity.java
@@ -1,29 +1,39 @@
 package com.example.quicoffee;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.Gravity;
+import android.view.View;
 import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
-import javax.microedition.khronos.opengles.GL;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
-public class MainActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
     private int usernameTextboxID;
     private int passwordTextboxID;
     private int mainActivityWitdh;
     private int mainActivityHeight;
     private LinearLayout linearLayout;
+    private FirebaseAuth auth;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        InititalVariablesOfLocalActivity();
         BuildActivityUI();
 
     }
@@ -50,6 +60,7 @@ public class MainActivity extends AppCompatActivity {
         mainActivityWitdh = getResources().getDisplayMetrics().widthPixels;
         mainActivityHeight = getResources().getDisplayMetrics().heightPixels;
         linearLayout = findViewById(R.id.linear_layout);
+        auth = FirebaseAuth.getInstance();
     }
     private EditText CreateEditText(LinearLayout.LayoutParams lparams) {
         //Set EditText Setting
@@ -63,7 +74,7 @@ public class MainActivity extends AppCompatActivity {
     private void addLoginButton() {
         //Set Button Settings
         Button loginButton = new Button(this);
-        //loginButton.setOnClickListener(this);
+        loginButton.setOnClickListener(this);
         loginButton.setText(Global_Variable.LOGIN);
         LinearLayout.LayoutParams loginButtonLayoutParams =
                 new LinearLayout.LayoutParams((int)(mainActivityWitdh *0.5),mainActivityHeight/20);
@@ -84,5 +95,46 @@ public class MainActivity extends AppCompatActivity {
         textView.setTextColor(getApplication().getResources().getColor(R.color.colorBlack));
         textView.setTextSize(mainActivityWitdh/40);
         return textView;
+    }
+
+    @Override
+    public void onClick(View v) {
+        String email = ((EditText)findViewById(usernameTextboxID)).getText().toString();
+        final String password = ((EditText)findViewById(passwordTextboxID)).getText().toString();
+
+        if (TextUtils.isEmpty(email)) {
+            Toast.makeText(getApplicationContext(), Global_Variable.MISSING_EMAIL_INFORMATION, Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if (TextUtils.isEmpty(password)) {
+            Toast.makeText(getApplicationContext(), Global_Variable.MISSING_PASSWORD_INFORMATION, Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        //authenticate user
+        auth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        // If sign in fails, display a message to the user. If sign in succeeds
+                        // the auth state listener will be notified and logic to handle the
+                        // signed in user can be handled in the listener.
+                        //progressBar.setVisibility(View.GONE);
+                        if (!task.isSuccessful()) {
+                            // there was an error
+                            if (password.length() < 6) {
+                                ((EditText)findViewById(passwordTextboxID)).setError(getString(Global_Variable.MINIMUM_PASSWORD));
+                            } else {
+                                Toast.makeText(LoginActivity.this, Global_Variable.AUTH_FAILED, Toast.LENGTH_LONG).show();
+                            }
+                        } else {
+                            //todo change to which activity
+                            Intent intent = new Intent(LoginActivity.this, LoginActivity.class);
+                            startActivity(intent);
+                            finish();
+                        }
+                    }
+                });
     }
 }
