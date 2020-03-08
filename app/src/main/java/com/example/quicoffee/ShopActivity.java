4d74935415ab8ded37bb8274a2ed8c671aa494ab
DorEbert <dorebert1@gmail.com>
2020-03-08 23:15:22 +0200

    add into try/catch

--- app/src/main/java/com/example/quicoffee/ShopActivity.java
+++ app/src/main/java/com/example/quicoffee/ShopActivity.java
@@ -223,18 +223,14 @@ public class ShopActivity extends AppCompatActivity implements OnMapReadyCallbac
 
         try {
             addresses = geocoder.getFromLocation(latitude, longitude, 1); // Here 1 represent max location result to returned, by documents it recommended 1 to 5
+            String address = addresses.get(0).getAddressLine(0); // If any additional address line present than only, check with max available address lines by getMaxAddressLineIndex()
+            return address;
         } catch (IOException e) {
             Toast.makeText(getApplicationContext(), Global_Variable.COULD_NOT_DETECT_LOCATION, Toast.LENGTH_SHORT).show();
             return "Address was Not found";
         }
         //todo return a string that present the full address of the coordinates
-        String address = addresses.get(0).getAddressLine(0); // If any additional address line present than only, check with max available address lines by getMaxAddressLineIndex()
-        String city = addresses.get(0).getLocality();
-        String state = addresses.get(0).getAdminArea();
-        String country = addresses.get(0).getCountryName();
-        String postalCode = addresses.get(0).getPostalCode();
-        String knownName = addresses.get(0).getFeatureName(); // Only if available else return NULL
-        return address;
+
     }
     //add shop on click
     @Override
